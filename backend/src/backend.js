5ccadc628cb2ea1992927cff7af6462aff565448
Swati Verma <swativerma@Swatis-MacBook-Pro.local>
2022-10-22 14:05:41 -0230

    GOAL: Create a Database Schema

--- backend/src/backend.js
+++ backend/src/backend.js
@@ -1,5 +1,27 @@
+// import DB from "sqlite3";
+
 const express = require("express")
 const app = express()
+
+const sqlite3 = require("sqlite3").verbose();
+// let sql;
+
+const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+})
+
+const mainSchema = () => {
+    db.prepare(
+       `CREATE TABLE IF NOT EXISTS mainTable (
+        id INTEGER PRIMARY KEY,
+        product TEXT NOT NULL,
+        price INTEGER DEFAULT 0,
+        quantity INTEGER DEFAULT 0
+    )`
+    ).run();
+}
+
+mainSchema();
 
 app.use(express.static("public"))
 
@@ -8,3 +30,6 @@ const userRouter = require('./routes/users')
 app.use("/users", userRouter)
 
 app.listen(3001)
+
+
+
