18fcc82f799da7ab0e2f9ce4863d1f6c32b484bb
Mitchell Hynes <ecumene@users.noreply.github.com>
2022-10-22 14:40:53 -0230

    Made some changes to backend.js

--- backend/src/backend.js
+++ backend/src/backend.js
@@ -1,35 +1,26 @@
-// import DB from "sqlite3";
-
-const express = require("express")
-const app = express()
-
 const sqlite3 = require("sqlite3").verbose();
-// let sql;
+const express = require("express");
+const app = express();
 
-const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.error(err.message);
-})
+const db = new sqlite3.Database("./platform.db");
 
 const mainSchema = () => {
-    db.prepare(
-       `CREATE TABLE IF NOT EXISTS mainTable (
+  db.prepare(
+    `CREATE TABLE IF NOT EXISTS mainTable (
         id INTEGER PRIMARY KEY,
         product TEXT NOT NULL,
         price INTEGER DEFAULT 0,
         quantity INTEGER DEFAULT 0
     )`
-    ).run();
-}
+  ).run();
+};
 
 mainSchema();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-const userRouter = require('./routes/users')
+const userRouter = require("./routes/users");
 
-app.use("/users", userRouter)
+app.use("/users", userRouter);
 
-app.listen(3001)
-
-
-
+app.listen(3001);
