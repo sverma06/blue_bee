70ad3420a293ddfdad379277bde5b6bd4cd35b17
Swati Verma <swativerma@Swatis-MacBook-Pro.local>
2022-11-05 13:55:30 -0230

    Bcrypt password and compare password

--- backend/src/backend.js
+++ backend/src/backend.js
@@ -1,13 +1,18 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
+var session = require('express-session')
+const bcrypt = require("bcrypt");
+
 const app = express();
 
 const db = new sqlite3.Database("./platform.db");
 
+// tables
 const mainSchema = () => {
   db.prepare(
     `CREATE TABLE IF NOT EXISTS user (
-        user_id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT DEFAULT 0,
         create_at TEXT NOT NULL DEFAULT (datetime('now', 'localtime'))
@@ -31,10 +36,99 @@ const mainSchema = () => {
 
 mainSchema();
 
-app.use(express.static("public"));
-
 const userRouter = require("./routes/users");
+
+app.use(express.static("public"));
 
 app.use("/users", userRouter);
 
-app.listen(3001);
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+// // get form for login
+// app.get("/login", function (req, res) {
+//   res.send(
+//     '<form action="/login" method="post">' +
+//       'Username: <input name="username"><br>' +
+//       'Password: <input name="password" type="password"><br>' +
+//       '<input type="submit" text="Login"></form>'
+//   );
+// });
+
+// // get form for signup
+// app.get("/signup", function (req, res) {
+//   res.send(
+//     '<form action="/signup" method="post">' +
+//       'Username: <input name="username"><br>' +
+//       'Email: <input name="email" type="email"><br>' +
+//       'Password: <input name="password" type="password"><br>' +
+//       'Password again: <input name="password" type="password"><br>' +
+//       '<input type="submit" text="Signup"></form>'
+//   );
+// });
+
+// post hashed password for signup
+app.post("/signup", express.urlencoded({ extended: false }),
+  async function (req, res) {
+    console.log(req.body)
+    const [password, confirmPassword] = req.body.password
+    if(password != confirmPassword) {
+      res.status(400).send()
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      let newUser = `INSERT INTO user(username, email, password) VALUES (?,?,?)`;
+      db.run(newUser, [req.body.username, req.body.email, hashedPassword], (err) => {
+        if(err) {
+          throw err;
+        }
+        req.session.user = { username: req.body.username };
+        res.redirect("/");
+      });
+    } catch (e) {
+      console.log(e);
+      res.redirect("/signup");
+    }
+  }
+);
+
+// post for login
+app.post("/login", express.urlencoded({ extended: false }),
+  async function (req, res) {
+db.get( "SELECT * FROM user WHERE username = ?", req.body.username,
+  (err, row) => {
+    if (err) throw err;
+    try {
+      bcrypt.compare(req.body.password, row.password, (err, same) => {
+        if (err) throw err;
+        console.log(req.body.password, row.password);
+        if (same) {
+          res.send("welcome!");
+          req.session.user = { username: row.username };
+        } else {
+          res.send("incorrect password/username");
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+});
+});
+
+// logout
+app.get("/logout", (req,res) => {
+  req.session.user = null;
+  res.status(200).send();
+})
+
+//Error handling
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send('Something is broken');
+});
+
+app.listen(3001, () => console.log('Running on 3001'));
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 const Login = () => {
+
   return (
     <div className="form">
-      <form>
-      <h2>Login</h2>
+      <form method="POST" action="/login">
+        <h2>Login</h2>
         <div className="userid">
-          <label className="label">User ID </label>
-          <input className="input" type="text" placeholder="User ID"></input>
+          <label className="label">Username </label>
+          <input
+            className="input"
+            type="text"
+            placeholder="Username"
+            name="username"
+          ></input>
         </div>
         <div className="password">
           <label className="label">Password </label>
@@ -15,12 +21,14 @@ const Login = () => {
             className="input"
             type="password"
             placeholder="Password"
+            name="password"
           ></input>
         </div>
         <div>
           <button className="button">Login</button>
         </div>
       </form>
+      <button className="button">Don't have an account? Signup here</button>
     </div>
   );
 };
