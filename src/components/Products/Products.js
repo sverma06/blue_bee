08b629cc2dd5bb3827d89cd4cd19414c30ec57e5
Swati Verma <swativerma@Swatis-MacBook-Pro.local>
2022-11-30 18:28:18 -0330

    Fetch GET /products on the frontend

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import AddProducts from './AddProducts';
+import React, { useState, useEffect} from "react";
 
 const Products = () => {
-    return (
-        <>
-            <label>Product Entered</label>
-            <AddProducts />
-        </>
-        
-    );
+  const [list, setList] = useState([]);
 
-}
+  useEffect(() => {
+    const url = 'http://localhost:3000/products';
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            console.log(json.list);
+            setList(json.list);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <>
+      <label>Product Entered</label>
+      <ul>
+        {list?.map((item) => (
+          <li key={item.item}>{item.item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Products;
