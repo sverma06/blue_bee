53f67c1656d12fc75d2eac978364a81233688f67
Swati Verma <verma.swati0610@gmail.com>
2022-12-28 19:27:02 -0500

    Frontend for signing up

---
+++ src/components/signup/signup.js
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// const signup = () => {
+//   return (
+//     <div className="box">
+//       <form>
+//         <h2>Create account</h2>
+//         <div className="userid">
+//           <label>User ID: </label>
+//           <input type="text" placeholder="User ID"></input>
+//         </div>
+//         <div className="email">
+//           <label>Email: </label>
+//           <input type="text" placeholder="Email"></input>
+//         </div>
+//         <div className="password">
+//           <label>Password: </label>
+//           <input type="password" placeholder="Password"></input>
+//         </div>
+//         <div className="password">
+//           <label>Password again: </label>
+//           <input type="password" placeholder="Password"></input>
+//         </div>
+//         <div>
+//           <button className="signup_button">Signup</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default signup;
