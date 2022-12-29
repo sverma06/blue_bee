12e52f41e43bd2d74f95370c0ac79a3679a404e3
Swati Verma <verma.swati0610@gmail.com>
2022-12-28 19:27:04 -0500

    Shopping cart UI

---
+++ src/components/Header/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const ShoppingCart = () => {
+    return (
+        <ShoppingCartIcon />
+    );
+};
+
+export default ShoppingCart;
