1759f193acfd50d4cef854cb007bfc4750018c7d
Swati Verma <verma.swati0610@gmail.com>
2022-12-28 19:27:03 -0500

    Implemented useForm for signup form

--- src/components/Logout/LogoutButton.js
+++ src/components/Logout/LogoutButton.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './LogoutButton.css';
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
+    const navigate = useNavigate();
+
+    const onLogout = () => { navigate("/login", { replace: true })}
+
     return (
         <div className='logoutContainer'>
-            <button className='logoutButton'>Logout</button>
+            <button className='logoutButton' onClick={onLogout}>Logout</button>
         </div>
     )
 }
